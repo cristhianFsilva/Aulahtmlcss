aac200adf43b6533f0ce3036164d3376be63cba3
Cristhian Silva <cristhianx13@gmail.com>
2019-09-18 22:24:19 -0300

    Criando pagina inicial para aprender HTML, CSS, Javascript

---
+++ Front-end/js/inicial.js
@@ -0,0 +1,20 @@
+//troca o titulo
+function myFunction2() {
+  document.getElementById("demo").innerHTML = "CORNO";
+}
+//troca o background do body
+function trocaBackCor(){
+    document.getElementById("bodyId").classList = "bodyClass2";
+    
+
+}
+function trocaBackCor2(){
+    document.getElementById("bodyId").classList = "bodyClass";
+    
+
+}
+function escreverSaudacao(nome){
+    document.getElementById("saudacao").innerHTML = "Boa noite " +nome;
+
+
+}
